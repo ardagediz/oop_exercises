b351607dfce5f9b644a7c5c713be211b80b5b574
Arda Gediz <144790644+ardagediz@users.noreply.github.com>
2024-03-18 16:22:41 +0000

    New Exercise

---
+++ AlarmClock.java
@@ -0,0 +1,41 @@
+public class AlarmClock {
+    private int alarmHours;
+    private int alarmMinutes;
+
+    public AlarmClock() {
+
+    }
+
+    public AlarmClock(int h, int m) {
+
+    }
+
+    public AlarmClock(int h, int m, int s) {
+
+    }
+
+    public int getAlarmHours() {
+
+    }
+
+    public int getAlarmMinutes() {
+
+    }
+
+    public setAlarm(int h, int m) {
+
+    }
+
+    public boolean isRinging() {
+
+    }
+
+    public display() {
+        
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
