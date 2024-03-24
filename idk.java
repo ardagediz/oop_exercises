7814b449daf92bb926293ec7f584c13b168f1521
Arda Gediz <144790644+ardagediz@users.noreply.github.com>
2024-03-24 19:47:11 +0000

    Change

--- idk.java
+++ idk.java
@@ -1,5 +1,14 @@
 public class idk {
     public static void main(String[] args) {
-        if (args.length)
+        if (args.length <= 0) {
+            System.err.println("No args given");
+            System.exit(1);
+        } 
+
+        String password = args[0];
+    }
+
+    public static boolean enoughCharacters() {
+        
     }
 }
